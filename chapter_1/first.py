63b9e68d75482a1293c5a3c1efb3b21910a5b65c
gannuengineer <engineerganesh18@gmail.com>
2024-11-18 21:24:42 +0530

    added files

---
+++ chapter_1/first.py
@@ -0,0 +1,13 @@
+# printing a string, koi bhi sentence ya word likhna hota hai to string ka hi use karte hain
+print("Hello world")
+print("My age is 19")
+
+# aek hi line mein print krne ke liye 
+print("Hello world.","My age is 19")
+
+# agar bracket nahi lagayenge to uska sum y jo operation lagate hain wo print ayega 
+# agar bracket laga diya toh same cheej print ho jayegi..
+print("35+5")
+print(35+5)
+
+
---
+++ chapter_1/variable.py
@@ -0,0 +1,33 @@
+# variable print
+name = "Ganesh"
+rollnumber = 2024041224
+branch = 23.6
+age = True
+a = None
+
+print(name)
+print(rollnumber)
+print(branch)
+
+print("My name is :", name)
+print("My Rollnumber :", rollnumber)
+print("My branch is :", branch)
+
+
+# type 
+
+# string           isko hum "" , '' , '"'" me print kar sakte hain...
+print(type(name))
+
+# int 
+print(type(rollnumber))
+
+# float
+print(type(branch))
+
+# bolean   True Or False first letter should be in capital 
+print(type(age))
+
+# None     N should be in capital letter 
+print(type(a))
+
